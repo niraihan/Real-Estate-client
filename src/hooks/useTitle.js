4b880c1e85d6bdbb7c7db6a0406d69f8c7cf6705
niraihan <raihan14un@gmail.com>
2025-08-29 15:41:21 +0600

    some file is updated

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,19 +1,19 @@
-// src/hooks/useTitle.js
-import { useEffect } from "react";
+// // src/hooks/useTitle.js
+// import { useEffect } from "react";
 
-/**
- * Custom hook to set dynamic page title with a base app name.
- * @param {string} title - Page specific title
- * @param {string} base - Base title (default: "UrbanNest")
- */
-const useTitle = (title, base = "UrbanNest") => {
-  useEffect(() => {
-    if (title) {
-      document.title = `${title} | ${base}`;
-    } else {
-      document.title = base;
-    }
-  }, [title, base]);
-};
+// /**
+//  * Custom hook to set dynamic page title with a base app name.
+//  * @param {string} title - Page specific title
+//  * @param {string} base - Base title (default: "UrbanNest")
+//  */
+// const useTitle = (title, base = "UrbanNest") => {
+//   useEffect(() => {
+//     if (title) {
+//       document.title = `${title} | ${base}`;
+//     } else {
+//       document.title = base;
+//     }
+//   }, [title, base]);
+// };
 
-export default useTitle;
+// export default useTitle;

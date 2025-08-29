9dfd678bdd2b565784e2b2a63d5e7f5a6d025697
niraihan <raihan14un@gmail.com>
2025-08-29 15:42:10 +0600

    Us title is ok

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,19 +1,19 @@
-// // src/hooks/useTitle.js
-// import { useEffect } from "react";
 
-// /**
-//  * Custom hook to set dynamic page title with a base app name.
-//  * @param {string} title - Page specific title
-//  * @param {string} base - Base title (default: "UrbanNest")
-//  */
-// const useTitle = (title, base = "UrbanNest") => {
-//   useEffect(() => {
-//     if (title) {
-//       document.title = `${title} | ${base}`;
-//     } else {
-//       document.title = base;
-//     }
-//   }, [title, base]);
-// };
+import { useEffect } from "react";
 
-// export default useTitle;
+/**
+ * Custom hook to set dynamic page title with a base app name.
+ * @param {string} title - Page specific title
+ * @param {string} base - Base title (default: "UrbanNest")
+ */
+const useTitle = (title, base = "UrbanNest") => {
+  useEffect(() => {
+    if (title) {
+      document.title = `${title} | ${base}`;
+    } else {
+      document.title = base;
+    }
+  }, [title, base]);
+};
+
+export default useTitle;
